5b664210696b0725e92164f2209ec8d2e0f5396c
faizanwar2015 <resalatanwar@gmail.com>
2016-09-25 20:11:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/resalat/a2fragments/fragment1.java
@@ -0,0 +1,29 @@
+package com.example.resalat.a2fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by resalat on 9/24/2016.
+ */
+public class fragment1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+    }
+
+
+
+
+}
